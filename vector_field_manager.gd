2a725c9b317757a6f80b3b00e59a279757edd795
codingtime217 <flashwardy04@gmail.com>
2025-08-17 15:50:27 +0100

    Basic flux calc, seems to be wrong direction though

--- vector_field_manager.gd
+++ vector_field_manager.gd
@@ -12,61 +12,65 @@ extends Node2D
 func _generate_grid() -> void:
 	for i in width:
 		for j in height:
-			grid[Vector2(i,j)] = {"colour" = Color(0,i/width,j/height),"vector" = Vector2(16,16), "value" = i*10,"conductivity"	= 10.0}
+			grid[Vector2(i,j)] = {"colour" = Color(0,i/width,0),"vector" = Vector2(16,16), "value" = i*10,"conductivity"	= 1.0,}
 
 func _draw() -> void:
-	print("drawing")
+	
 	var offset = get_viewport_rect().size/2
 	#draws all the tiles, offsetting such that the centre tile is the in middle of the screen
 	for coord in grid.keys():
 		var screen_pos : Vector2 = (coord-Vector2(width/2,height/2)) * dimensions + offset
 		var screen_size := Vector2(32,32)
 		draw_rect(Rect2(screen_pos, screen_size), grid[coord]["colour"])
-		draw_line((screen_pos+Vector2(16,16))-grid[coord]["vector"]/2,(screen_pos+Vector2(16,16))+grid[coord]["vector"]/2,Color(1,1,1))
+		draw_line((screen_pos+Vector2(16,16)),(screen_pos+Vector2(16,16))+grid[coord]["vector"]/2,Color(1,1,1))
 func _ready() -> void:
 	_generate_grid()
 	
 	pass # Replace with function body.
 
 func _getneighbours(position = Vector2()):
-	func _getneighbour(position):
-		var value = grid.get(position+Vector2(i,j),null)
-			if value == null:
-				return("")
-			else:
-				return value
-	var neighbours = [] # 
-	#fetches the 4 tiles around the position given, in anticlockwise order from the left hand side
-	for i in range(-1,2):
-		neighbours.append(_getneightbour(position + Vector2(i,0)))	
-	for j in range(-1,2):
-			neighbours.append(_getneightbour(position + Vector2(0,j)))	
 	
+	var value
+	const fetchVectors = [Vector2(1,0),Vector2(-1,0),Vector2(0,1),Vector2(0,-1)]
+	var neighbours = []
+	#fetches the 4 tiles around the position given, in anticlockwise order from the left hand side
+	for i in fetchVectors:
+		value = grid.get(position+i,null)
+		if value != null:
+			neighbours.append(value)
+		else:
+			neighbours.append({"conductivity" = 0.0})
 	return neighbours
 
-func _getflux(position1=Vector2(),position2 = Vector2()):
+func _getflux(tile1,tile2):
 	#finding heat flux using equation q = -kdeltaT, finding flux from 1 to 2
 	#q is flux, k is conductivty, detlaT is differecne in temp/distance	
 	var distance = 0.1 #o.1 m
-	var bit1 = grid[position1]
-	var bit2 = grid[position2]
-	if bit1["conductity"] == bit2["conductivity"]:
+	if tile1["conductivity"] == tile2["conductivity"]:
 		#smae material, this simplifies things
-		var dT = (bit2["value"] - bit1["value"])/distance
-		return dT*-bit1["conductivity"]
+		var dT = (tile2["value"] - tile1["value"])/distance
+		return dT*-tile1["conductivity"]
 	else:
 		return 0 #i aint working this out yer
 
 func _overallFlux(pos1= Vector2()):
-	
-	pass
-
+	var neighbours = _getneighbours()
+	var fluxes = []
+	for i in neighbours:
+		fluxes.append(_getflux(grid[pos1],i))
+	var net = Vector2()
+	net.x = fluxes[0] - fluxes[1]
+	net.y = fluxes[2] - fluxes[3]
+	print(net)
+	grid[pos1]["vector"] = net/10
 
 func _update():
 	for i in width:
 		for j in height:
+			_overallFlux(Vector2(i,j))
+	queue_redraw()
 			#iterates through whole grid
-			_getneighbours(Vector2(i,j))
+			
 
 
 
