424b23322962ac2e6ed9dc886acf9730f6205c65
codingtime217 <182775961+codingtime217@users.noreply.github.com>
2025-08-17 15:23:19 +0100

    forgor to push these

--- vector_field_manager.gd
+++ vector_field_manager.gd
@@ -1,7 +1,9 @@
 extends Node2D
+#note on sim, each cell is treated as a 0.001m^3 cube (0.1m by 0.1m by 0.1m)
+
 
 @export var dimensions = Vector2(32,32) #dimensions of each cell
-@export var height = 10.0
+@export var height = 10.0 #rectangle of cell dimesnon
 @export var width = 10.0
 @export var grid = {}
 
@@ -10,11 +12,12 @@ extends Node2D
 func _generate_grid() -> void:
 	for i in width:
 		for j in height:
-			grid[Vector2(i,j)] = {"colour" = Color(0,i/width,j/height),"vector" = Vector2(16,16), "value" = i}
+			grid[Vector2(i,j)] = {"colour" = Color(0,i/width,j/height),"vector" = Vector2(16,16), "value" = i*10,"conductivity"	= 10.0}
 
 func _draw() -> void:
 	print("drawing")
 	var offset = get_viewport_rect().size/2
+	#draws all the tiles, offsetting such that the centre tile is the in middle of the screen
 	for coord in grid.keys():
 		var screen_pos : Vector2 = (coord-Vector2(width/2,height/2)) * dimensions + offset
 		var screen_size := Vector2(32,32)
@@ -26,21 +29,44 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _getneighbours(position = Vector2()):
-	var neighbours = [[],[],[]]
-	
-	for i in range(-1,2):
-		for j in range(-1,2):
-
-			var value = grid.get(position+Vector2(i,j),null)
+	func _getneighbour(position):
+		var value = grid.get(position+Vector2(i,j),null)
 			if value == null:
-				neighbours[i].append("")
+				return("")
 			else:
-				neighbours[i].append(value)
+				return value
+	var neighbours = [] # 
+	#fetches the 4 tiles around the position given, in anticlockwise order from the left hand side
+	for i in range(-1,2):
+		neighbours.append(_getneightbour(position + Vector2(i,0)))	
+	for j in range(-1,2):
+			neighbours.append(_getneightbour(position + Vector2(0,j)))	
+	
 	return neighbours
+
+func _getflux(position1=Vector2(),position2 = Vector2()):
+	#finding heat flux using equation q = -kdeltaT, finding flux from 1 to 2
+	#q is flux, k is conductivty, detlaT is differecne in temp/distance	
+	var distance = 0.1 #o.1 m
+	var bit1 = grid[position1]
+	var bit2 = grid[position2]
+	if bit1["conductity"] == bit2["conductivity"]:
+		#smae material, this simplifies things
+		var dT = (bit2["value"] - bit1["value"])/distance
+		return dT*-bit1["conductivity"]
+	else:
+		return 0 #i aint working this out yer
+
+func _overallFlux(pos1= Vector2()):
+	
+	pass
+
 
 func _update():
 	for i in width:
-		print(_getneighbours(Vector2(i,0)))
+		for j in height:
+			#iterates through whole grid
+			_getneighbours(Vector2(i,j))
 
 
 
