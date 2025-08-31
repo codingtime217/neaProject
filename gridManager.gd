0a1e7903b38628505a94e10ace28b99878e2411c
codingtime217 <flashwardy04@gmail.com>
2025-08-31 14:49:02 +0100

    Refactor seemingly successful, but having issue redrawing after updates

--- gridManager.gd
+++ gridManager.gd
@@ -10,46 +10,34 @@ var tile = load("res://tile.gd")
 
 # Called when the node enters the scene tree for the first time.
 
-func _generate_grid() -> void:
-	for i in width:
-		for j in height:
-			grid[Vector2(i,j)] = {"colour" = Color(0,i/width,0),"vector" = Vector2(0,0), "value" = 10*i*j,"conductivity"	= 1,"specificCapacity" = 100.0}
 			
 func _generateGrid() -> void:
-	var newcoord #stores the key for each tile about to be made
+	var offset = get_viewport_rect().size/2 - Vector2(width*16, height *16)
+	var newcoord  #stores the key for each tile about to be made
+	var newtile #stores the tile breifly
 	for i in width:
 		for j in height:
 			newcoord = Vector2(i,j) #sets the key/coordinates
-			grid[newcoord] = tile.new("water",newcoord*32) # instantiates a new water tile, and gives it its global position (the key * 32 as 32 by 32 sqaures)
+			newtile = tile.new()
+			newtile.setup("water",newcoord*16)
+			add_child(newtile)
+			grid[newcoord] = newtile
+			 # instantiates a new water tile, and gives it its global position (the key * 32 as 32 by 32 sqaures)
 	
-func _draw() -> void:
-	
-	var offset = get_viewport_rect().size/2
-	#draws all the tiles, offsetting such that the centre tile is the in middle of the screen
-	for coord in grid.keys():
-		var screen_pos : Vector2 = (coord-Vector2(width/2,height/2)) * dimensions + offset
-		var screen_size := Vector2(32,32)
-		draw_rect(Rect2(screen_pos, screen_size), grid[coord]["colour"])
-		draw_line((screen_pos+Vector2(16,16)),(screen_pos+Vector2(16,16))+grid[coord]["vector"],Color(1,1,1))
+#func _draw() -> void:
+	#
+	#var offset = get_viewport_rect().size/2
+	##draws all the tiles, offsetting such that the centre tile is the in middle of the screen
+	#for coord in grid.keys():
+		#var screen_pos : Vector2 = (coord-Vector2(width/2,height/2)) * dimensions + offset
+		#var screen_size := Vector2(32,32)
+		#draw_rect(Rect2(screen_pos, screen_size), grid[coord]["colour"])
+		#draw_line((screen_pos+Vector2(16,16)),(screen_pos+Vector2(16,16))+grid[coord]["vector"],Color(1,1,1))
 func _ready() -> void:
 	_generateGrid()
 	
 	pass # Replace with function body.
 
-func _getneighbours(location = Vector2()):
-	var value
-	const fetchVectors = [Vector2(1,0),Vector2(-1,0),Vector2(0,1),Vector2(0,-1)]
-	var neighbours = []
-	var toFetch = Vector2(0,0)
-	#fetches the 4 tiles around the position given, in anticlockwise order from the left hand side
-	for i in fetchVectors:
-		toFetch = location + i
-		value = grid.get(toFetch,null)
-		if value != null:
-			neighbours.append(value)
-		else:
-			neighbours.append({"conductivity" = 0.0})
-	return neighbours
 
 func _getNeighbours(location = Vector2()):
 	var value
@@ -73,12 +61,14 @@ func _getNeighbours(location = Vector2()):
 func _getHeatFlux(tile1,tile2):
 	#finding heat flux using equation q = -kdeltaT, finding flux from 1 to 2
 	#q is flux, k is conductivty, detlaT is differecne in temp/distance	
+	if (tile1 == null) or (tile2 == null) or tile1.compound == "void" or tile2.compound == "void":
+		return 0
 	var distance = 0.1 #o.1 m
-	if tile1["conductivity"] == tile2["conductivity"]:
+	if tile1.conductivity == tile2.conductivity:
 		#smae material, this simplifies things
 		#print("not coid")
-		var dT = (tile1["value"] - tile2["value"])/distance # should find flux into tile1 (positive is tile1 going up)
-		return dT*-tile1["conductivity"]
+		var dT = (tile1.temp - tile2.temp)/distance # should find flux into tile1 (positive is tile1 going up)
+		return dT*-tile1.conductivity
 	else:
 		#print("void tile")
 		return 0.0 #i aint working this out yer
@@ -86,7 +76,7 @@ func _getHeatFlux(tile1,tile2):
 func _overallFlux(pos1= Vector2()): #define flux from left to right as positive and right to left as negative
 	#flux down is pos, flux up is negative
 	#thus aligns with screen coords
-	var neighbours = _getneighbours(pos1)
+	var neighbours = _getNeighbours(pos1)
 	var fluxes = []
 	var net  = 0.0
 	var flux = 0.0
@@ -103,23 +93,23 @@ func _overallFlux(pos1= Vector2()): #define flux from left to right as positive 
 func _update():
 	var netFlux
 	var fluxes
-	var newGrid = grid.duplicate(true) #make a copy to overwrite everything at once
+	
 	for currentCell in grid.keys():
 		#_overallFlux(Vector2(i,j))
 		#fluxIn =_summedFlux(currentCell)
 		fluxes =  _overallFlux(currentCell) 
 		netFlux= fluxes[0]#get the overall change
 		
-		newGrid[currentCell]["value"] += netFlux/newGrid[currentCell]["specificCapacity"] #change the temp
-		newGrid[currentCell]["vector"] = fluxes[1] #update vector
-		newGrid[currentCell]["colour"] = Color(0,newGrid[currentCell]["value"]/180,0) #update colour, need better func to have a wider range of colour than black to green
-	grid = newGrid #overwrite grid
-	queue_redraw() #redraw
+		grid[currentCell].temp += netFlux/grid[currentCell].specificHeatCap #change the temp
+		grid[currentCell].vector = fluxes[1] #update vector
+
+
+	#queue_redraw() #redraw
 
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	#_update()
+	_update()
 	pass
--- tile.gd
+++ tile.gd
@@ -3,12 +3,13 @@ var compound = "water"
 @export var vector = Vector2(0,0) #starts as nothing, used for visualising flux atm
 @export var temp = 0.0 #in kelvin
 @export var conductivity = 1.0 #in Wm^-1K^-1 a proportionality constant on thermal flux
-@export var specficHeatCap = 100.0 #in KJ^-1kg^-1
+@export var specificHeatCap = 100.0 #in KJ^-1kg^-1
 @export var colour = Color(0,0,0)
 @export var density = 1000.0 #kgm^-3
 @export var mass = 0.0
+var screen_size := Vector2(32,32)
 var dimensions = Vector2(32,32)
-
+var offset = Vector2(100,100)
 
 #func _ready():
 	#offset = get_viewport_rect().size/2
@@ -34,28 +35,29 @@ func _updateColour():#make colour chnage with temp, try and accurate blackbody
 
 
 
-func _init(mat = "void",pos = Vector2(0,0)):
-	print(mat)
-	self.position = (pos-Vector2(0.5,0.5))*dimensions
+func setup(mat = "void",pos = Vector2(0,0)):
+	position = pos
 	var properties = materialsDict.get(mat,null)
 	if properties == null:
 		properties = materialsDict["water"]
 	compound = mat
+	temp = pos.x
 	conductivity = properties["conductivity"]
-	specficHeatCap = properties["specificHeat"]
+	specificHeatCap = properties["specificHeat"]
 	density = properties["density"]
-	mass = density / 1000.0 #as volume is 1/1000 m^3
-	colour = Color(0,pos.x/1000,pos.y/1000)
+	mass = density / 1000.0  #as volume is 1/1000 m^3
+	colour = Color(0,temp/300,0)
+	
 
 
 
 func _draw() -> void:
-	var screen_size := Vector2(32,32)
+	colour = Color(0,temp/300,0)
 	draw_rect(Rect2(position, screen_size), colour)
 	#draw_line((global_position,(global_position+Vector2(16,16))+vector,Color(1,1,1))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	#initiate("water")
+	queue_redraw()
 	pass
