d45ca783089cc3f5d1a91d8c6a9d4f83de83434e
codingtime217 <flashwardy04@gmail.com>
2025-11-16 13:28:46 +0000

    debugging shader so it actually runs

--- shaderssssss.gd
+++ shaderssssss.gd
@@ -11,26 +11,29 @@ var input := PackedFloat32Array([1,[[1,1,1,1],[1,1,1,1],[1,1,1,1]],
 var inputBytes := input.to_byte_array()
 var output := PackedFloat32Array([[[1,1,1,1],[1,1,1,1],[1,1,1,1]],
 
-				[[1,1,1,1],[10,1,1,1],[1,1,1,1]],
+				[[1,1,1,1],[1,1,1,1],[1,1,1,1]],
 				[[1,1,1,1],[1,1,1,1],[1,1,1,1]]])
 var outputBytes := output.to_byte_array()
 var inBuffer := rd.uniform_buffer_create(inputBytes.size(),inputBytes)
 var outBuffer := rd.storage_buffer_create(outputBytes.size(),outputBytes)
-
 var uniformIn := RDUniform.new()
-var uniformInSet
+var uniformOut := RDUniform.new()
+var uniformSet : RID
 var pipeline := rd.compute_pipeline_create(shader)
 var compute_list := rd.compute_list_begin()
-
+var run = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	uniformIn.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
 	uniformIn.binding = 0 # this needs to match the "binding" in our shader file
 	uniformIn.add_id(inBuffer)
-	uniformInSet = rd.uniform_set_create([uniformIn], shader, 0)
+	uniformOut.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	uniformOut.binding = 1
+	uniformOut.add_id(outBuffer)
+	uniformSet = rd.uniform_set_create([uniformIn,uniformOut],shader,0)
 	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
-	rd.compute_list_bind_uniform_set(compute_list, uniformInSet, 0)
-	rd.compute_list_dispatch(compute_list, 5, 1, 1)
+	rd.compute_list_bind_uniform_set(compute_list, uniformSet, 0)
+	rd.compute_list_dispatch(compute_list, 1, 3, 1)
 	rd.compute_list_end()
 	
 
@@ -38,4 +41,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_lta: float) -> void:
+	if not run:
+		rd.submit()
+		rd.sync()
+		run = true
 	pass
--- thermal.glsl
+++ thermal.glsl
@@ -2,46 +2,46 @@
 #version 450
 // setup stuff
 
-layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+layout(local_size_x = 3, local_size_y = 1, local_size_z = 1) in;
 // experiment with other values to find a more appropriate number
 
-layout(set = 0, binding = 0 ,std430) restrict uniform buffer InBuffer {
+layout(set = 0, binding = 0 , std430) restrict buffer InBuffer {
     float distance;
-    float[4] grid[][]; //have first value be thermal energy, then conductivity, specHeatCap, then mass
+    float[4] grid[3][3]; //have first value be thermal energy, then conductivity, specHeatCap, then mass
 }
 inBuffer;
 
-layout(set = 1, binding = 0, std430) restrict OutBuffer{
-    float[4] newGrid[][];
+layout(set = 0, binding = 1, std430) restrict buffer OutBuffer{
+    float[4] newGrid[3][3];
 }
 outBuffer;
 
 
-float getFlux(float[4] tile1, float[4] tile2)
+float getFlux(float tile1[4], float tile2[4])
 {
-    float tile1Temp = (tile1[1]*tile1[3])/tile1[0][2]
-    float tile2Temp = (tile2[1]*tile2[3])/tile2[0][2]
-    float flux = -tile1[1]*((tile1Temp - tile2Temp)/inBuffer.distance)
-    return flux
+    float tile1Temp = (tile1[1]*tile1[3])/tile1[2];
+    float tile2Temp = (tile2[1]*tile2[3])/tile2[2];
+    float flux = -tile1[1]*((tile1Temp - tile2Temp)/inBuffer.distance);
+    return flux;
 }
 
 
 //for each invocation (tile)
-void main {
-    grid = inBuffer.grid
-    tiles[5] = [grid[gl_GlobalInvocationID.x][gl_GlobalInvocationID.y],grid[gl_GlobalInvocationID.x+1][gl_GlobalInvocationID.y],grid[gl_GlobalInvocationID.x-1][gl_GlobalInvocationID.y],grid[gl_GlobalInvocationID.x][gl_GlobalInvocationID.y+1],grid[gl_GlobalInvocationID.x][gl_GlobalInvocationID.y-1]]
-    if gl_GlobalInvocationID.x == grid.length():
+void main() {
+    float[4] grid[3][3] = inBuffer.grid;
+    float[4] tiles = (grid[gl_GlobalInvocationID.x][gl_GlobalInvocationID.y],grid[gl_GlobalInvocationID.x+1][gl_GlobalInvocationID.y],grid[gl_GlobalInvocationID.x-1][gl_GlobalInvocationID.y],grid[gl_GlobalInvocationID.x][gl_GlobalInvocationID.y+1],grid[gl_GlobalInvocationID.x][gl_GlobalInvocationID.y-1]);
+    if (gl_GlobalInvocationID.x == grid.length())
         tiles[1] = [0.0,0.0,0.0,0.0];
-    else if gl_GlobalInvocationID.x == 0:
+    else if (gl_GlobalInvocationID.x == 0)
        tiles[2] = [0.0,0.0,0.0,0.0];
-    if gl_GlobalInvocationID.y == grid.length()
+    if (gl_GlobalInvocationID.y == grid.length())
         tiles[3] = [0.0,0.0,0.0,0.0];
-    else if gl_GlobalInvocationID == 0:
+    else if (gl_GlobalInvocationID == 0)
         tiles[4] = [0.0,0.0,0.0,0.0];
     
     float[4] fluxes = [getFlux(tiles[0],tiles[1]),getFlux(tiles[0],tiles[2]),getFlux(tiles[0],tiles[3]).getFlux(tiles[0],tiles[4])];
     float netFlux = fluxes[0] + fluxes[1] + fluxes[2] + fluxes[3];
     //net flux is the sum of fluxes between the cell and its neighbours
     float newData = tiles[0][0] + netFlux;
-    outBuffer.newGrid[gl_GlobalInvocationID.x][gl_GlobalInvocationID.y] = newData
+    outBuffer.newGrid[gl_GlobalInvocationID.x][gl_GlobalInvocationID.y] = newData;
 }
