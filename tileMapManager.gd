dde55d307e0d59611febf4fdaaad3fc45e63e709
codingtime217 <182775961+codingtime217@users.noreply.github.com>
2026-02-04 21:23:26 +0000

    Started UI stuff

--- tileMapManager.gd
+++ tileMapManager.gd
@@ -1,13 +1,13 @@
 extends TileMapLayer
 
-var mousePos : Vector2
-# Called when the node enters the scene tree for the first time.
+var mousePos : Vector2 #current mouse position, self explanatory
+var currentMatAtlas : Vector2i # will be used to store the current material's atlas coords
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	mousePos = get_local_mouse_position()
 	if Input.is_action_pressed("left_click"):
 		var tilePos = local_to_map(mousePos)
