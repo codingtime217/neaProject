72f9329b2dc7affa45bf24cd74774287916e02b5
codingtime217 <flashwardy04@gmail.com>
2026-02-05 21:23:43 +0000

    YEAHHHHH

--- matSelected.gd
+++ matSelected.gd
@@ -1,14 +1,13 @@
-extends Node2D
-var UI = preload("res://UIeditor.tscn")
+extends ItemList
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().root.add_child.call_deferred(UI.instantiate())
-	
+	var tileMap := get_node(^"/root/Editor/tileManager")
+	tileMap._link_signals()
 	pass # Replace with function body.
 
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
--- tileMapManager.gd
+++ tileMapManager.gd
@@ -1,19 +1,31 @@
 extends TileMapLayer
 
 var mousePos : Vector2 #current mouse position, self explanatory
-var currentMatAtlas : Vector2i # will be used to store the current material's atlas coords
+var currentMatAtlas : Vector3i # will be used to store the current material's atlas + coords 
+# x is the atlas no, y and z are the atlas coords
+
 func _ready() -> void:
+	#call_deferred("_link_signals")
 	pass # Replace with function body.
 
+func _link_signals():
+	var UINode = get_node(^"/root/UIEditor/cont/ItemList")
+	UINode.item_selected.connect(selected)
+	pass
+
+func selected(index : int):
+	currentMatAtlas.x = index
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	mousePos = get_local_mouse_position()
 	if Input.is_action_pressed("left_click"):
 		var tilePos = local_to_map(mousePos)
-		set_cell(tilePos,0,Vector2i(0,0))
+		set_cell(tilePos,currentMatAtlas.x,Vector2i(currentMatAtlas.y,currentMatAtlas.z))
 	elif Input.is_action_pressed("right_click"):
 		var tilePos = local_to_map(mousePos)
 		set_cell(tilePos,-1)
 	pass
-	
+
+#func _notification(what: int) -> void:
