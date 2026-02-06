ae103d34db463928bd1aee82e51bfef9e0bed3c2
codingtime217 <182775961+codingtime217@users.noreply.github.com>
2026-02-06 09:48:06 +0000

    Small ui work

--- matSelected.gd
+++ matSelected.gd
@@ -4,5 +4,6 @@ extends ItemList
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var tileMap := get_node(^"/root/Editor/tileManager")
-	tileMap._link_signals()
+	if tileMap != null:
+		tileMap._link_signals()
 	pass # Replace with function body.
