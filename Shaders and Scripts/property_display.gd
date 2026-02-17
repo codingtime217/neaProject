e5e30e83df509e2b7648a51e5e8b26b485026a66
codingtime217 <flashwardy04@gmail.com>
2026-02-17 17:52:59 +0000

    Undid OOP

--- Shaders and Scripts/property_display.gd
+++ Shaders and Scripts/property_display.gd
@@ -1,34 +1,34 @@
 extends Control
 
-class Display extends Control:
-	var parent : Node
-	var label : String
-	var value
-	var mat : String
-	var labelBox
-	var valueBox
+
+var parent : Node
+var label : String
+var value
+var mat : String
+var labelBox
+var valueBox
 
 
-	# Called when the node enters the scene tree for the first time.
-	func _ready() -> void:
-		labelBox = get_node("propertyLabel")
-		valueBox = get_node("propertyData")
-		parent = get_node("../..")
-		parent.connect("freeDisplays",queue_free)
-		mat = parent.mat
-		labelBox.text = label
-		valueBox.text = str(value)
-		pass # Replace with function body.
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	labelBox = get_node("propertyLabel")
+	valueBox = get_node("propertyData")
+	parent = get_node("../..")
+	parent.connect("freeDisplays",queue_free)
+	mat = parent.mat
+	labelBox.text = label
+	valueBox.text = str(value)
+	pass # Replace with function body.
 
-	func cleanup(text : String) -> String:
-		var cleanedText = ""
-		var text2 = text.split("_")
-		for i in text2:
-			cleanedText = cleanedText + " " + i.capitalise()
+func cleanup(text : String) -> String:
+	var cleanedText = ""
+	var text2 = text.split("_")
+	for i in text2:
+		cleanedText = cleanedText + " " + i.capitalise()
 		cleanedText.strip_edges()
-		return cleanedText + ":"
+	return cleanedText + ":"
 
 
 	# Called every frame. 'delta' is the elapsed time since the previous frame.
-	func _process(delta: float) -> void:
-		pass
+func _process(delta: float) -> void:
+	pass
